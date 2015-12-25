513cb1f8d34689cf7edd1b5d12e702b8239c849f
Saurabh Jain <sjain@shoprunner.com>
2015-12-24 21:22:42 -0800

    model

---
+++ model.py
@@ -0,0 +1,11 @@
+class Listing():
+    def __init__(self, listing_id, street, status, price, bedrooms, bathrooms, sq_ft, longitude, latitude):
+            self.id = listing_id
+            self.street = street
+            self.status = status
+            self.price = price
+            self.bedrooms = bedrooms
+            self.bathrooms = bathrooms
+            self.sq_ft = sq_ft
+            self.longitude = longitude
+            self.latitude = latitude
