77bf39de79e250679e386748cd58e1cc40ecc39e
Saurabh Jain <sjain@shoprunner.com>
2015-12-24 20:55:07 -0800

    load listings

--- service.py
+++ service.py
@@ -1,13 +1,23 @@
 import gevent.monkey; gevent.monkey.patch_all()
-
+from opendoor.model import Listing
 import bottle
 import httplib
 import logging
 import os
+import csv
+import urllib2
 
 app = bottle.Bottle()
 
 log = logging.getLogger(__name__)
+
+def load_listings(csv_url):
+    listings = []
+    content = urllib2.urlopen(csv_url)
+    cr = csv.reader(content)
+
+    for row in cr:
+        logging.info(row)
 
 
 @app.get('/health')
@@ -23,3 +33,6 @@ if __name__ == '__main__':
         port=os.environ.get('PORT', 5000),
         server='gevent'
     )
+
+    # load the csv file into the listing model
+    listings = load_listings('https://s3.amazonaws.com/opendoor-problems/listings.csv')
